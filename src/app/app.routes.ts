2bd57da70128e53b691d797e0c10abf6d504feb0
AXEL-HR <axel.seba.hr@gmail.com>
2026-01-05 02:45:44 -0300

    Update home route to new component path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./paginas/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },
-  {
-    path: 'pag-frase',
-    loadComponent: () => import('./paginas/frases/pag-frase/pag-frase.page').then( m => m.PagFrasePage)
   },
   {
     path: 'citas',
